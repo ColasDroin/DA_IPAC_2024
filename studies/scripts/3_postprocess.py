3a8ab4b9d697918fbbc803ac94d8c496e7d9f2e3
Colas Droin <colasdroin@gmail.com>
2024-03-18 10:20:27 +0100

    Added bbb analysis

--- studies/scripts/3_postprocess.py
+++ studies/scripts/3_postprocess.py
@@ -16,7 +16,7 @@ print("Analysis of output simulation files started")
 start = time.time()
 
 # Load Data
-study_name = "tune_scan_end_of_levelling_higher_emit"
+study_name = "bbb_end_of_levelling"
 fix = f"/../scans/{study_name}"
 root = tree_maker.tree_from_json(fix[1:] + "/tree_maker.json")
 # Add suffix to the root node path to handle scans that are not in the root directory
@@ -113,7 +113,9 @@ if df_lost_particles.empty:
     print("No unstable particles found, the output dataframe will be empty.")
 
 # Group by working point (Update this with the knobs you want to group by !)
-group_by_parameters = ["name base collider", "qx", "qy"]
+# group_by_parameters = ["name base collider", "qx", "qy"]
+group_by_parameters = ["name base collider", "i_bunch_b1", "i_bunch_b2"]
+
 # We always want to keep beam in the final result
 group_by_parameters = ["beam"] + group_by_parameters
 l_parameters_to_keep = [
